ad9a6c14ee74674fe0ef88c91aca7df63e883ab8
SPEED <rqmtw4058@gmail.com>
2025-03-18 16:19:01 +0200

    Update install-wordpress.sh

--- ubuntu/install-wordpress.sh
+++ ubuntu/install-wordpress.sh
@@ -1,108 +1,66 @@
 #!/bin/bash
 
-## This script assumes you already set up your SSH access, firewall, etc. 
-## If not, run /ubuntu/setup.sh script first.
-##
-## Usage:
-## sudo bash -c "$(curl -sS https://raw.githubusercontent.com/l-s-I-I/scripts/refs/heads/master/ubuntu/install-wordpress.sh)"
-##
-
 set -e
 
 if [[ "$EUID" -ne 0 ]]; then
-  echo "Error: Please run as root with sudo."
-  exit
+  echo -e "\e[33mError: Please run as root with sudo.\e[0m"
+  exit 1
 fi
 
 WP_DB_NAME=wordpress
 WP_DB_ADMIN_USER=wpadmin
 
-echo "HOSTNAME:"
+echo -e "\e[33mHOSTNAME:\e[0m"
 read HOSTNAME
-echo
 
-echo "MySQL password for wpadmin user:"
-echo "> 8 chars, including numeric, mixed case, and special characters"
+echo -e "\e[33mMySQL password for wpadmin user:\e[0m"
+echo -e "> 8 chars, including numeric, mixed case, and special characters"
 read -s MYSQL_WP_ADMIN_USER_PASSWORD
-echo
 
-## Ubuntu updates and dependencies
-echo "Starting system update and dependency installation..."
-apt-get update
-apt-get -y upgrade
-apt-get -y autoremove
-echo "System update and dependency installation completed."
+echo -e "\e[33mStarting system update and dependency installation...\e[0m"
+apt-get update && apt-get -y upgrade && apt-get -y autoremove
 
-## Wordpress dependencies (LEMP stack)
-echo "Installing WordPress dependencies (LEMP stack)..."
-apt-get install -y \
-nginx \
-mysql-server \
-php-curl \
-php-mysql \
-php-gd \
-php-intl \
-php-mbstring \
-php-soap \
-php-xml \
-php-xmlrpc \
-php-zip \
-php-fpm
-echo "WordPress dependencies installation completed."
+apt-get install -y nginx mysql-server php-fpm php-mysql certbot python3-certbot-nginx
 
-## Determine installed PHP-FPM version
+echo -e "\e[33mSystem update and dependency installation completed.\e[0m"
+
 PHP_FPM_VERSION=$(php --version | grep -oP '^PHP \K[0-9]+\.[0-9]+' | head -1)
 PHP_FPM_SOCKET="/run/php/php${PHP_FPM_VERSION}-fpm.sock"
 
-# nginx setup
-echo "Configuring Nginx..."
 cat > /etc/nginx/sites-available/$HOSTNAME <<EOF
-resolver 8.8.8.8 8.8.4.4;
 server {
   listen 80;
-  root /var/www/html/wordpress/blog;
-  index  index.php index.html index.htm;
   server_name $HOSTNAME;
-  error_log /var/log/nginx/error.log;
-  access_log /var/log/nginx/access.log;
-  client_max_body_size 100M;
+  root /var/www/html/wordpress/blog;
+  index index.php index.html index.htm;
   location / {
     try_files \$uri \$uri/ /index.php?\$args;
   }
   location ~ \.php$ {
     include snippets/fastcgi-php.conf;
     fastcgi_pass unix:$PHP_FPM_SOCKET;
-    fastcgi_param   SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+    fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
   }
 }
 EOF
 
-rm -f /etc/nginx/sites-enabled/default
 ln -sf /etc/nginx/sites-available/$HOSTNAME /etc/nginx/sites-enabled/
-service nginx reload
-echo "Nginx configuration completed."
+systemctl reload nginx
 
-## MySQL setup
-echo "Running mysql_secure_installation..."
-mysql_secure_installation <<EOF
-n # Skip setting the root password (auth_socket in use)
-Y # Remove anonymous users for security
-Y # Disallow root login remotely
-Y # Remove test database and access to it
-Y # Reload privilege tables to apply changes
-EOF
-echo "mysql_secure_installation completed."
+echo -e "\e[33mConfiguring SSL certificate...\e[0m"
+certbot --nginx -d $HOSTNAME --non-interactive --agree-tos -m your-email@example.com --redirect
 
-## MySQL setup for WordPress
-echo "Configuring MySQL for WordPress..."
+echo -e "\e[33mSSL setup completed!\e[0m"
+
+echo -e "\e[33mConfiguring MySQL for WordPress...\e[0m"
 mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS ${WP_DB_NAME};
 CREATE USER '${WP_DB_ADMIN_USER}'@'localhost' IDENTIFIED BY '${MYSQL_WP_ADMIN_USER_PASSWORD}';
-GRANT ALL ON ${WP_DB_NAME}.* TO '${WP_DB_ADMIN_USER}'@'localhost'
+GRANT ALL ON ${WP_DB_NAME}.* TO '${WP_DB_ADMIN_USER}'@'localhost';
+FLUSH PRIVILEGES;
 EOF
-echo "MySQL configuration for WordPress completed."
 
-echo "Installing WordPress 6.7.2..."
+echo -e "\e[33mInstalling WordPress 6.7.2...\e[0m"
 sudo mkdir -p /var/www/html/wordpress/src
 sudo mkdir -p /var/www/html/wordpress/blog
 cd /var/www/html/wordpress/src
@@ -111,106 +69,29 @@ sudo tar -xvf wordpress-6.7.2.tar.gz
 sudo mv wordpress-6.7.2.tar.gz wordpress-`date "+%Y-%m-%d"`.tar.gz
 sudo mv wordpress/* ../blog/
 sudo chown -R www-data:www-data /var/www/html/wordpress/blog
-echo "WordPress 6.7.2 installation completed."
 
-## Set up wp-config.php (current as of Wordpress 5.8)
-echo "Setting up wp-config.php..."
+echo -e "\e[33mWordPress 6.7.2 installation completed.\e[0m"
+
 WP_SECURE_SALTS="$(curl -s https://api.wordpress.org/secret-key/1.1/salt/)"
-
 WP_CONFIG_FILE=/var/www/html/wordpress/blog/wp-config.php
 cat > "${WP_CONFIG_FILE}" <<EOF
 <?php
-/**
- * The base configuration for WordPress
- *
- * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the web site, you can copy this file to "wp-config.php"
- * and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://wordpress.org/support/article/editing-wp-config-php/
- *
- * @package WordPress
- */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
 define( 'DB_NAME', '${WP_DB_NAME}' );
-
-/** MySQL database username */
 define( 'DB_USER', '${WP_DB_ADMIN_USER}' );
-
-/** MySQL database password */
 define( 'DB_PASSWORD', '${MYSQL_WP_ADMIN_USER_PASSWORD}' );
-
-/** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
-
-/** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
-
-/**#@+
- * Authentication unique keys and salts.
- *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
 ${WP_SECURE_SALTS}
-
-/**#@-*/
-
-/**
- * WordPress database table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
 \$table_prefix = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
 define( 'WP_DEBUG', false );
-
-/* Add any custom values between this line and the "stop editing" line. */
-
-
-
-/* That's all, stop editing! Happy publishing. */
-
-/** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+    define( 'ABSPATH', __DIR__ . '/' );
 }
-
-/** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
 EOF
 
-sudo chown -R www-data:www-data "${WP_CONFIG_FILE}"
-echo "wp-config.php setup completed."
+chown -R www-data:www-data "${WP_CONFIG_FILE}"
 
-echo "WordPress installation and configuration completed successfully!"
+echo -e "\e[33mWordPress installation and SSL setup completed successfully!\e[0m"
